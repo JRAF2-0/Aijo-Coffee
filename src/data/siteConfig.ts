352d332f4f6d12701d52e51adcf711a42aa8166a
Jhon Rowel Abines <jraf2003@gmail.com>
2026-07-20 22:04:53 +0800

    change social med links

--- src/data/siteConfig.ts
+++ src/data/siteConfig.ts
@@ -6,16 +6,16 @@ export const siteConfig = {
 
   address: "W9RV+2J3, Poblacion East, Moalboal, Cebu",
   phone: "+63 912 345 6789",
-  email: "jraf@aijocoffee.com",
+  email: "jraf@jrafdrinks.com",
 
   hours: [
     { day: "Monday - Sunday", time: "7:00 AM - 9:00 PM" },
   ],
 
   socials: {
-    facebook: "https://facebook.com/aijocoffee",
-    instagram: "https://instagram.com/aijocoffee",
-    tiktok: "https://tiktok.com/@aijocoffee",
+    facebook: "https://facebook.com/jrafdrinks",
+    instagram: "https://instagram.com/jrafdrinks",
+    tiktok: "https://tiktok.com/@jrafdrinks",
   },
 
   mapEmbedUrl: "https://www.google.com/maps?q=W9RV%2B2J3+Poblacion+East,+Moalboal,+Cebu&output=embed",
